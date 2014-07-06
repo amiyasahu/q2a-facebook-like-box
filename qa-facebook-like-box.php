499c08c92dbbf5d881348764e22396032a131d59
Amiya Sahu <amiya053@gmail.com>
2014-07-06 11:50:07 +0530

    Code Improvements

--- qa-facebook-like-box.php
+++ qa-facebook-like-box.php
@@ -76,6 +76,7 @@
                                     'type'  => 'text',
                                     'tags'  => 'name="flb_page_url"',
                                     'value' =>  qa_opt('flb_page_url'),
+                                    'note'  =>  qa_lang('flb_like_box/ur_fb_page_url_note'),
                     ),
                     'flb_like_box_colorscheme' => array(
                                     'label' => qa_lang('flb_like_box/colorscheme_label'),
@@ -155,44 +156,56 @@
 
 		function output_widget($region, $place, $themeobject, $template, $request, $qa_content)
 		{
-
-			      $widget_opt  = qa_get_options(array('flb_page_url','flb_like_box_colorscheme','flb_like_box_header','flb_like_box_show_border','flb_like_box_show_faces','flb_like_box_data_stream','flb_like_box_height','flb_like_box_width'));
-            $fb_page_url = $this->get_fb_settings($widget_opt , 'url') ;	
+            $has_error     = false ; 
+            $error_message = "" ;
+            $widget_opt    = qa_get_options(array('facebook_app_id' , 'flb_page_url','flb_like_box_colorscheme','flb_like_box_header','flb_like_box_show_border','flb_like_box_show_faces','flb_like_box_data_stream','flb_like_box_height','flb_like_box_width'));
+            $fb_page_url   = $this->get_fb_settings($widget_opt , 'url') ;	
             
-            if (!$fb_page_url) {
-                  return false;
+            if (empty($fb_page_url)) {
+                  $has_error = true ;
+                  $error_message = qa_lang('flb_like_box/plz_provide_fb_url') ;
             }
 
-            // get the other settings 
-            $colorscheme =  $this->get_fb_settings($widget_opt , 'colorscheme') ; 
-            $header      =  $this->get_fb_settings($widget_opt , 'header') ; 
-            $show_border =  $this->get_fb_settings($widget_opt , 'show_border') ; 
-            $show_faces  =  $this->get_fb_settings($widget_opt , 'show_faces') ; 
-            $stream      =  $this->get_fb_settings($widget_opt , 'stream') ; 
-            $height      =  $this->get_fb_settings($widget_opt , 'height') ; 
-            $width       =  $this->get_fb_settings($widget_opt , 'width') ; 
-
-            $data['href']        = 'data-href="'.$fb_page_url.'"' ;
-            $data['force_wall']  = 'data-force-wall="false"' ;
-            $data['colorscheme'] = 'data-colorscheme="'.$colorscheme.'"' ;
-            $data['header']      = 'data-header="'.$header.'"' ;
-            $data['show_border'] = 'data-show-border="'.$show_border.'"' ;
-            $data['show_faces']  = 'data-show-faces="'.$show_faces.'"' ;
-            $data['stream']      = 'data-stream="'.$stream.'"' ;
-            $data['height']      = 'data-height="'.$height.'"' ;
-            $data['width']       = 'data-width="'.$width.'"' ;
-            $data_str = implode(' ', $data ) ;
-            
-            $fb_like_box =   '<div class="fb-like-box" '.$data_str.'> </div>'  ;
-            
-            $facebook_app_id = qa_opt('facebook_app_id');
-
-            if (!$facebook_app_id) {
-                // if the facebook app id is not set set it with app id given by the Facebook 
-                $facebook_app_id = "576492145800361" ; 
+            if (!$has_error) {
+                $themeobject->output($this->get_facebook_like_box($widget_opt));
+            }else {
+               $themeobject->output('<div class="qa-sidebar error" style="color:red;">'.$error_message.'</div>');
             }
+            
+		}
+    function get_facebook_like_box($widget_opt)
+    {
+        // get the other settings 
+        $facebook_app_id =  $this->get_fb_settings($widget_opt , 'facebook_app_id') ; 
+        $fb_page_url     =  $this->get_fb_settings($widget_opt , 'url') ;
+        $colorscheme     =  $this->get_fb_settings($widget_opt , 'colorscheme') ; 
+        $header          =  $this->get_fb_settings($widget_opt , 'header') ; 
+        $show_border     =  $this->get_fb_settings($widget_opt , 'show_border') ; 
+        $show_faces      =  $this->get_fb_settings($widget_opt , 'show_faces') ; 
+        $stream          =  $this->get_fb_settings($widget_opt , 'stream') ; 
+        $height          =  $this->get_fb_settings($widget_opt , 'height') ; 
+        $width           =  $this->get_fb_settings($widget_opt , 'width') ; 
 
-            ob_start();
+        $data['href']        = 'data-href="'.$fb_page_url.'"' ;
+        $data['force_wall']  = 'data-force-wall="false"' ;
+        $data['colorscheme'] = 'data-colorscheme="'.$colorscheme.'"' ;
+        $data['header']      = 'data-header="'.$header.'"' ;
+        $data['show_border'] = 'data-show-border="'.$show_border.'"' ;
+        $data['show_faces']  = 'data-show-faces="'.$show_faces.'"' ;
+        $data['stream']      = 'data-stream="'.$stream.'"' ;
+        $data['height']      = 'data-height="'.$height.'"' ;
+        $data['width']       = 'data-width="'.$width.'"' ;
+        
+        $data_str        = implode(' ', $data ) ;
+        $fb_like_box     = '<div class="fb-like-box" '.$data_str.'> </div>'  ;
+        $facebook_app_id = qa_opt('facebook_app_id');
+
+        if (!$facebook_app_id) {
+            // if the facebook app id is not set set it with app id given by the Facebook 
+            $facebook_app_id = "576492145800361" ; 
+        }
+
+        ob_start();
             ?>
               <!--  widget start  -->
                   <div id="fb-root"></div>
@@ -209,55 +222,59 @@
               <!--  widget ends  -->
             <?php
 
-            $themeobject->output(ob_get_clean());
-                       
-		}
-    
+        return ob_get_clean();
+    }
+
 		function get_fb_settings($widget_opt , $opt )
        {
             $value = "" ;
              switch ($opt) {
                    case 'url':
                    case 'href':
-                         $value = isset($widget_opt['flb_page_url']) ? $widget_opt['flb_page_url'] : "" ;
+                         $value = isset($widget_opt['flb_page_url']) && !empty($widget_opt['flb_page_url']) ? 'https://www.facebook.com/'.$widget_opt['flb_page_url'] : "" ;
                          break;
+                   
+                   case 'facebook_app_id':
+                         $value = isset($widget_opt['facebook_app_id']) && !empty($widget_opt['facebook_app_id']) ? $widget_opt['facebook_app_id'] : "" ;
+                         break;
+
                    case 'colorscheme':
-                         $value         = isset($widget_opt['flb_like_box_colorscheme']) ? $widget_opt['flb_like_box_colorscheme'] : "" ;
+                         $value         = isset($widget_opt['flb_like_box_colorscheme']) && !empty($widget_opt['flb_like_box_colorscheme']) ? $widget_opt['flb_like_box_colorscheme'] : "" ;
                          $allowed_value = array('light' , 'dark'); /*allow only these values*/
                          if (!$value || !in_array($value , $allowed_value )) {
                                $value = "light" ;
                          }
                          break;
                    case 'header':
-                         $value         = isset($widget_opt['flb_like_box_header']) ? $widget_opt['flb_like_box_header'] : "" ;
+                         $value         = isset($widget_opt['flb_like_box_header']) && !empty($widget_opt['flb_like_box_header']) ? $widget_opt['flb_like_box_header'] : "" ;
                          $allowed_value = array('true' , 'false'); /*allow only these values*/
                          if (!$value || !in_array($value , $allowed_value )) {
                                $value = "true" ;
                          }
                          break;
                    case 'show_border':
-                         $value         = isset($widget_opt['flb_like_box_show_border']) ? $widget_opt['flb_like_box_show_border'] : "" ;
+                         $value         = isset($widget_opt['flb_like_box_show_border']) && !empty($widget_opt['flb_like_box_show_border']) ? $widget_opt['flb_like_box_show_border'] : "" ;
                          $allowed_value = array('true' , 'false'); /*allow only these values*/
                          if (!$value || !in_array($value , $allowed_value )) {
                                $value = "true" ;
                          }
                          break;
                    case 'show_faces':
-                         $value         = isset($widget_opt['flb_like_box_show_faces']) ? $widget_opt['flb_like_box_show_faces'] : "" ;
+                         $value         = isset($widget_opt['flb_like_box_show_faces']) && !empty($widget_opt['flb_like_box_show_faces']) ? $widget_opt['flb_like_box_show_faces'] : "" ;
                          $allowed_value = array('true' , 'false'); /*allow only these values*/
                          if (!$value || !in_array($value , $allowed_value )) {
                                $value = "true" ;
                          }
                          break;
                    case 'stream':
-                         $value         = isset($widget_opt['flb_like_box_data_stream']) ? $widget_opt['flb_like_box_data_stream'] : "" ;
+                         $value         = isset($widget_opt['flb_like_box_data_stream']) && !empty($widget_opt['flb_like_box_data_stream']) ? $widget_opt['flb_like_box_data_stream'] : "" ;
                          $allowed_value = array('true' , 'false'); /*allow only these values*/
                          if (!$value || !in_array($value , $allowed_value )) {
                                $value = "false" ;
                          }
                          break;
                    case 'height':
-                         $value = isset($widget_opt['flb_like_box_height']) ? $widget_opt['flb_like_box_height'] : "" ;
+                         $value = isset($widget_opt['flb_like_box_height']) && !empty($widget_opt['flb_like_box_height']) ? $widget_opt['flb_like_box_height'] : "" ;
                          if ($this->get_fb_settings($widget_opt , "data_stream") && $this->get_fb_settings($widget_opt , "show_faces")   ) {
                                // if both are true min height is 556px
                                $min_height = 556 ;
@@ -274,7 +291,7 @@
                          }
                          break;
                    case 'width':
-                         $value     = isset($widget_opt['flb_like_box_width']) ? $widget_opt['flb_like_box_width'] : "" ;
+                         $value     = isset($widget_opt['flb_like_box_width']) && !empty($widget_opt['flb_like_box_width']) ? $widget_opt['flb_like_box_width'] : "" ;
                          $min_width = 190 ; /*allow only these values*/
                          if (!$value || $value < $min_width) {
                                $value = $min_width ;

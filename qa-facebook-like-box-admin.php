9fdb1bb36c34efd522d3c3e2162300b4058c0acc
Amiya Sahu <amiya053@gmail.com>
2014-07-13 16:09:17 +0530

    Liscence updated

--- qa-facebook-like-box-admin.php
+++ qa-facebook-like-box-admin.php
@@ -1,4 +1,29 @@
 <?php
+/*
+  Question2Answer (c) Gideon Greenspan
+  Google Plus Badge (c) Amiya Sahu (developer.amiya@outlook.com)
+
+	http://www.question2answer.org/
+
+	
+	File: qa-plugin/basic-adsense/qa-basic-adsense.php
+	Version: See define()s at top of qa-include/qa-base.php
+	Description: Widget module class for AdSense widget plugin
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
 	class qa_facebook_like_box_admin {
 		
 		const FACEBOOK_PAGE_URL         = 'flb_page_url' ;
